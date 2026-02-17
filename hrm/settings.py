91d4ba0b3b825597ac4a6632e3e711b52ce782a4
Milan <milanpanja731@gmail.com>
2026-02-17 23:02:48 +0530

    setup all folder structure

--- hrm/settings.py
+++ hrm/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.employees',
+    'apps.users',
+    'apps.departments',
+    'apps.attendance',
+    'apps.leave_management',
+    'apps.payroll',
 ]
 
 MIDDLEWARE = [
